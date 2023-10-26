b0cc6bf4c00c8c079b505414429f824dbed4ac6d
LuciaPuppo897 <alerome897@gmail.com>
2023-10-26 00:34:12 -0700

    add first tests files

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,62 @@
+##!/usr/bin/python3
+""" Unit tests for BaseModel class"""
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import json
+import os
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+    def test_unique_ids(self):
+        # Check that generated IDs are unique
+        other_base_model = BaseModel()
+        self.assertNotEqual(self.base_model.id, other_base_model.id)
+
+    def test_id_type(self):
+        # Check that the ID is a string
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_dates_format(self):
+        # Check the format of created_at and updated_at
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.assertEqual(datetime.now().strftime(date_format), self.base_model.created_at.strftime(date_format))
+        self.assertEqual(datetime.now().strftime(date_format), self.base_model.updated_at.strftime(date_format))
+
+    def test_save_updates_date(self):
+        # Check that the 'save' method updates the updated_at attribute
+        original_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(original_updated_at, self.base_model.updated_at)
+
+    def test_to_dict(self):
+        # Check the correctness of the 'to_dict' method
+        base_model_dict = self.base_model.to_dict()
+        self.assertEqual(base_model_dict["__class__"], "BaseModel")
+        self.assertIsInstance(base_model_dict["created_at"], str)
+        self.assertIsInstance(base_model_dict["updated_at"], str)
+        self.assertEqual(base_model_dict["created_at"], self.base_model.created_at.isoformat())
+        self.assertEqual(base_model_dict["updated_at"], self.base_model.updated_at.isoformat())
+
+    def test_json_serialization(self):
+        # Check if object can be converted to JSON and back
+        base_model_dict = self.base_model.to_dict()
+        base_model_json = json.dumps(base_model_dict)
+        new_base_model_dict = json.loads(base_model_json)
+        new_base_model = BaseModel(**new_base_model_dict)
+        self.assertEqual(self.base_model.id, new_base_model.id)
+        self.assertEqual(self.base_model.created_at, new_base_model.created_at)
+        self.assertEqual(self.base_model.updated_at, new_base_model.updated_at)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,58 @@
+##!/usr/bin/python3
+
+"""
+Unit tests for the FileStorage class
+"""
+
+import unittest
+import os
+from models import FileStorage
+from models.user import User
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Test cases for the FileStorage class.
+    """
+
+    def setUp(self):
+        self.storage = FileStorage()
+        self.user = User()
+
+    def tearDown(self):
+        try:
+            os.remove(self.storage._FileStorage__file_path)
+        except:
+            pass
+
+    def test_all(self):
+        """
+        Test the 'all' method
+        """
+        all_objects = self.storage.all()
+        self.assertEqual(type(all_objects), dict)
+        self.assertEqual(len(all_objects), 0)
+
+    def test_new(self):
+        """
+        Test the 'new' method
+        """
+        self.storage.new(self.user)
+        key = "{}.{}".format(self.user.__class__.__name__, self.user.id)
+        all_objects = self.storage.all()
+        self.assertIn(key, all_objects)
+
+    def test_save_reload(self):
+        """
+        Test the 'save' and 'reload' methods
+        """
+        self.storage.new(self.user)
+        self.storage.save()
+        new_storage = FileStorage()
+        new_storage.reload()
+        key = "{}.{}".format(self.user.__class__.__name__, self.user.id)
+        all_objects = new_storage.all()
+        self.assertIn(key, all_objects)
+
+if __name__ == '__main__':
+    unittest.main()
