3ad00fb5e89ed75826f5723588aff022254f1bc6
Nitsu47 <agustraversa2004@gmail.com>
2023-10-29 22:26:30 +0000

    items() deleted from for

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         dict_s = {}
         with open(self.__class__.__file_path, 'w', encoding='utf-8') as file:
-            for key in self.__class__.__objects.items():
+            for key in self.__class__.__objects:
                 dict_s[key] = self.__class__.__objects[key].to_dict()
             json.dump(dict_s, file, indent=4)
 
